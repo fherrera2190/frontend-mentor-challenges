fe7df93999be994f1ee68a7484aeb7b405d871b6
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-12-19 23:34:51 -0300

    add list links

--- url-shortening-api/src/components/LinkSection.tsx
+++ url-shortening-api/src/components/LinkSection.tsx
@@ -7,6 +7,39 @@ export const LinkSection = () => {
         <input type="text" placeholder="Shorten a link here..." />
         <button className="btn-rectangle">Shorten It!</button>
       </form>
+
+      <div className="link-list">
+        <div className="link-item">
+          <p className="link-original">
+            http://www.frontendmentor.io/challenges
+          </p>
+          <hr />
+          <div className="link-shortened">
+            <a href="#">www.short.ly/short-link</a>
+            <button className="btn-rectangle">Copy</button>
+          </div>
+        </div>
+        <div className="link-item">
+          <p className="link-original">
+            http://www.frontendmentor.io/challenges
+          </p>
+          <hr />
+          <div className="link-shortened">
+            <a href="#">www.short.ly/short-link</a>
+            <button className="btn-rectangle">Copy</button>
+          </div>
+        </div>
+        <div className="link-item">
+          <p className="link-original">
+            http://www.frontendmentor.io/challenges
+          </p>
+          <hr />
+          <div className="link-shortened">
+            <a href="#">www.short.ly/short-link</a>
+            <button className="btn-rectangle">Copy</button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
